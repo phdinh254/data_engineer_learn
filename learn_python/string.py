8b7aeac1667ce258965e6a9cb189c50d920cee23
DINH TRAN <147835496+puudynk05@users.noreply.github.com>
2026-09-17 20:12:17 +0700

    learn python

---
+++ learn_python/string.py
@@ -0,0 +1,21 @@
+print ("Dinh bá khí \nđẹp trai số 1!!!")
+
+print ("Mục tiêu 6 năm nữa: \"$100.000\"")
+
+channer_name = "Dinh"
+print (channer_name + " bá khí")
+
+channer_name = "siêu bá khí"
+print (channer_name.upper().isupper())
+
+channer_name = "siêu bá khí"
+print (len(channer_name))
+
+channer_name = "siêu bá khí"
+print (channer_name[0])
+
+channer_name = "siêu bá khí"
+print (channer_name.index("b"))
+
+channer_name = "siêu bá khí"
+print (channer_name.replace("khí", "dơ"))
---
+++ learn_python/variable.py
@@ -0,0 +1,10 @@
+character_name = "Dinh"
+character_age = 20.66
+
+print (f"Đây là 1 câu chuyên nhỏ về anh {character_name} đẹp trai")
+
+character_name = "Lov"
+
+print (f"Anh {character_name} năm nay đã {character_age} tuổi")
+print ("Anh ấy sống tại TP.HCM")
+print ("Anh ấy là lập trình viên giỏi")
